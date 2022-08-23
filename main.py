ee4ac6fb24d8e0d278d153fb9aa5dacc3f858b0b
yang20211127 <111723646+yang20211127@users.noreply.github.com>
2022-08-23 17:17:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
 da, wea, pro, ci, temperature, highest, lowest = get_weather(city[0])
-da, s_wea, s_pro, s_ci, s_temperature, s_highest, s_lowest = get_weather(city[1])
+da, s_wea, s_pro, s_ci, s_tem, s_highest, s_lowest = get_weather(city[1])
 data = {"date":{"value":da,"color":get_random_color()},"province":{"value":pro,"color":get_random_color()},"city":{"value":ci,"color":get_random_color()},"weather":{"value":wea,"color":get_random_color()},"temperature":{"value":temperature,"color":get_random_color()},"love_days":{"value":get_count(),"color":get_random_color()},"birthday_left":{"value":get_birthday(),"color":get_random_color()},"words":{"value":get_words(),"color":get_random_color()},"highest": {"value":highest,"color":get_random_color()},"lowest":{"value":lowest, "color":get_random_color()}}
 count = 0
 for user_id in user_ids:
